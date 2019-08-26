9ce4cfc76788ae6831ae8abcbfc05c6823a3c773
ramonuchoa386 <ramonuchoa386@gmail.com>
2019-08-25 23:35:08 -0300

    estilos do menu

--- template-parts/header/header-nav.php
+++ template-parts/header/header-nav.php
@@ -1,13 +1,16 @@
 <nav id="site-navigation" class="main-navigation">
+    <img class='logo' src="<?php echo get_template_directory_uri(); ?>/img/ympar-logo-vertical-black-text.png" alt="Ympar - Experi&ecirc;ncias Personalizadas" />
+
     <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
         <span class="menuBtnBar"></span>
         <span class="menuBtnBar"></span>
         <span class="menuBtnBar"></span>
     </button>
     <?php
-    wp_nav_menu( array(
-        'theme_location' => 'menu-1',
-        'menu_id'        => 'primary-menu',
-    ) );
+        wp_nav_menu( array(
+            'theme_location' => 'menu-1',
+            'menu_id'        => 'primary-menu',
+        ) );
     ?>
+
 </nav><!-- #site-navigation -->
