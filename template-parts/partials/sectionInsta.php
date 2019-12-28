cecb0f98119857ae36b335a6ed0558bafc2184d9
Ramon Uchoa <ramonuchoa386@gmail.com>
2019-12-28 19:03:15 -0300

    atualizando shortcode id plugin insta para prod

--- template-parts/partials/sectionInsta.php
+++ template-parts/partials/sectionInsta.php
@@ -2,7 +2,7 @@
     <h2>Instagram</h2>
     <div class='sectionWrapper'>
         <div id='instaFeed'>
-            <?php echo do_shortcode("[fts_instagram instagram_id=258487057 pics_count=6 type=user width=100% profile_wrap=no super_gallery=yes columns=6 force_columns=no space_between_photos=1px icon_size=65px hide_date_likes_comments=no]"); ?>
+            <?php echo do_shortcode("[fts_instagram instagram_id=16770674 pics_count=6 type=user width=100% profile_wrap=no super_gallery=yes columns=6 force_columns=no space_between_photos=1px icon_size=65px hide_date_likes_comments=no]"); ?>
         </div>
     </div>
 </section>
